aba987b8def5c56a4b2f6fed5b18f3fc655aa958
Mike McAllister <mike@logikbox.com>
2020-07-02 14:16:11 -0400

    main nav in

--- recipe-book/src/app/app.component.ts
+++ recipe-book/src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'recipe-book';
+  loadedFeature: string = 'recipe';
+
+  onNavigate(event: string) {
+    this.loadedFeature = event;
+  }
 }
--- recipe-book/src/app/header/header.component.ts
+++ recipe-book/src/app/header/header.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  @Output() featureSelected = new EventEmitter<string>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onSelect(where: string) {
+    console.log(where);
+    this.featureSelected.emit(where);
   }
-
 }
